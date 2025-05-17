273f00bc601caaee3ff9858a6a6d97faf90ab4d3
explodeTheComputer <dnfjddk2@gmail.com>
2025-05-18 01:24:21 +0900

    Retrofit 및 Gson 컨버터 라이브러리 추가
    - `app/build.gradle.kts` 파일에 Retrofit 및 Gson 컨버터 의존성 추가
    - `gradle/libs.versions.toml` 파일에 Retrofit 및 Gson 컨버터 버전 정보 추가
        - `retrofit = "3.0.0"`
        - `converterGson = "3.0.0"`

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,6 +76,8 @@ dependencies {
     implementation(libs.grpc.okhttp)
     implementation(libs.grpc.protobuf.lite)
     implementation(libs.grpc.stub)
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
