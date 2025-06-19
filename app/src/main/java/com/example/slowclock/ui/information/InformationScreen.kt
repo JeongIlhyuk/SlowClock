e96f59668f7fe25969193fea6d0045346e741155
LightlessStar <leesong1122@gmail.com>
2025-06-20 05:23:22 +0900

    파일 Refactor
    파일 이름 변경 및 기능에 따른 File 구조 Refactor

--- app/src/main/java/com/example/slowclock/ui/information/InformationScreen.kt
+++ app/src/main/java/com/example/slowclock/ui/information/InformationScreen.kt
@@ -2,19 +2,15 @@ package com.example.slowclock.ui.information
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.pullrefresh.PullRefreshIndicator
 import androidx.compose.material.pullrefresh.rememberPullRefreshState
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
-import com.example.slowclock.data.model.InfoData
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.slowclock.ui.information.component.InfoList
 
 
 @OptIn(ExperimentalMaterialApi::class)
@@ -34,7 +30,7 @@ fun InformationScreen(infoViewModel: InfoViewModel = viewModel()) {
         modifier = Modifier
             .fillMaxWidth()
     ){
-        InfoList(list=infoList)
+        InfoList(list = infoList)
         PullRefreshIndicator(
             refreshing = isLoading,
             state = pullRefreshState,
--- app/src/main/java/com/example/slowclock/ui/information/component/InformationCard.kt
+++ app/src/main/java/com/example/slowclock/ui/information/component/InformationCard.kt
@@ -1,10 +1,9 @@
-package com.example.slowclock.ui.information
+package com.example.slowclock.ui.information.component
 
 import android.content.Intent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -12,11 +11,8 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
